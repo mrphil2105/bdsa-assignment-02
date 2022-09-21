9a52343b52e2774c9c6cea0c365175e834d1623f
eikbo <eikb@itu.dk>
2022-09-21 22:03:43 +0200

    More Delegate

--- Assignment2.Models/Delegates.cs
+++ Assignment2.Models/Delegates.cs
@@ -1,8 +1,8 @@
 namespace Assignment2.Models;
 public class Delegates{
 public delegate void Reverse(string s);
-public delegate void Product(string s);
-public delegate void IsEqual(string s);
+public delegate decimal Product(decimal s, decimal r);
+public delegate bool IsEqual(int i, string s);
 
 
 }
--- Assignment2.Tests/DelegatesTests.cs
+++ Assignment2.Tests/DelegatesTests.cs
@@ -4,16 +4,53 @@ using Assignment2.Models;
 public class DelegatesTests
 {
     [Fact]
-    public void Test1()
+    public void TestReverse()
     {
         var word = "Hello World";
-       Delegates.Reverse why = s => {
-        foreach(char c in new string(s.ToCharArray().Reverse().ToArray())){
-            Console.WriteLine(c);
-        }
-       };
+        Delegates.Reverse why = s =>
+        {
+            foreach (char c in new string(s.ToCharArray().Reverse().ToArray()))
+            {
+                Console.WriteLine(c);
+            }
+        };
 
-       why(word);
+        why(word);
+
+    }
+
+    [Fact]
+    public void TestProduct()
+    {
+        decimal dec1 = 0.5M;
+        decimal dec2 = 2.5M;
+        Delegates.Product why = (s, r) =>
+        {
+            return s * r;
+        };
+
+        why(dec1, dec2).Should().Be(1.25M);
+
+    }
+
+    [Fact]
+    public void TestIsEqual()
+    {
+        var word = "0022";
+        Delegates.IsEqual why = (i, s) =>
+        {
+            if (int.Parse(s) == i)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        };
+
+        Assert.Equal(true, why(22, "0022"));
 
     }
 }
