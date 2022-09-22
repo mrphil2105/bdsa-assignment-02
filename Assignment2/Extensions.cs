5775457a116ccb152801eb8cc41d3bbc47a801c7
Philip Mørch <mrphil2105@gmail.com>
2022-09-22 14:19:17 +0200

    Add Flatten and Filter extension methods

--- Assignment2/Extensions.cs
+++ Assignment2/Extensions.cs
@@ -2,6 +2,28 @@ namespace Assignment2;
 
 public static class Extensions
 {
+    public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> items)
+    {
+        foreach (var enumerable in items)
+        {
+            foreach (var item in enumerable)
+            {
+                yield return item;
+            }
+        }
+    }
+
+    public static IEnumerable<T> Filter<T>(this IEnumerable<T> items, Predicate<T> predicate)
+    {
+        foreach (var item in items)
+        {
+            if (predicate(item))
+            {
+                yield return item;
+            }
+        }
+    }
+
     public static bool IsSecure(this Uri uri) => uri.Scheme == Uri.UriSchemeHttps;
 
     public static int WordCount(this string str) =>
