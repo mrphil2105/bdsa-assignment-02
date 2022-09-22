13c31a6a7fda44fdaa36ad06502bbd5418a027af
Silke Bonnen <silke.bonnen@gmail.com>
2022-09-22 13:15:37 +0200

    test passing

--- Assignment2.Tests/QueriesTests.cs
+++ Assignment2.Tests/QueriesTests.cs
@@ -3,14 +3,84 @@ namespace Assignment2.Tests;
 public class QueriesTests
 {
     [Fact]
-    public void Rowling_query()
+    public void Rowling_query_linq()
     {
         // Given
         var wiz = Queries.wizardsByRowlingLinq();
-    
-        // When
-    
+
         // Then
-        wiz.Should().BeEquivalentTo(new List<string>{"Albus Dumbledore", "Severus Snape", "Harry Potter", "Draco Malfoy", "Ron Weasley", "Hermione Granger"});
+        wiz.Should().BeEquivalentTo(new List<string> { "Albus Dumbledore", "Severus Snape", "Harry Potter", "Ron Weasley", "Ron Weasley", "Hermione Granger" });
+    }
+
+    [Fact]
+    public void Rowling_query_extension_methods()
+    {
+        // Given
+        var wiz = Queries.wizardsByRowlingEx();
+
+        // Then
+        wiz.Should().BeEquivalentTo(new List<string> { "Albus Dumbledore", "Severus Snape", "Harry Potter", "Ron Weasley", "Ron Weasley", "Hermione Granger" });
+    }
+
+    [Fact]
+    public void First_sith_linq()
+    {
+        // Given
+        var wiz = Queries.firstSithLordLinq();
+
+        // Then
+        wiz.Should().Be(1977);
+    }
+
+    [Fact]
+    public void First_sith_ex()
+    {
+        // Given
+        var wiz = Queries.firstSithLordEx();
+
+        // Then
+        wiz.Should().Be(1977);
+    }
+
+    [Fact]
+    public void Distinct_Harry_Potter_linq()
+    {
+        // Given
+        var wiz = Queries.uniqueListHarryPotterLinq();
+
+        // Then
+        wiz.Should().BeEquivalentTo(new List<(string, int)>{("Albus Dumbledore", 2001), ("Severus Snape", 2001),
+                ("Harry Potter", 2001), ("Ron Weasley", 2001), ("Hermione Granger", 2001)});
+    }
+
+    [Fact]
+    public void Distinct_Harry_Potter_extension_methods()
+    {
+        // Given
+        var wiz = Queries.uniqueListHarryPotterEx();
+
+        // Then
+        wiz.Should().BeEquivalentTo(new List<(string, int)>{("Albus Dumbledore", 2001), ("Severus Snape", 2001),
+                ("Harry Potter", 2001), ("Ron Weasley", 2001), ("Hermione Granger", 2001)});
+    }
+
+    [Fact]
+    public void wizardNames_linq()
+    {
+        // Given
+        var wiz = Queries.wizardNamesLinq();
+
+        // Then
+        wiz.Should().BeEquivalentTo(new List<string> { "Gandalf", "Michael Mouse", "Sauron", "Albus Dumbledore", "Harry Potter", "Hermione Granger", "Ron Weasley", "Ron Weasley", "Severus Snape", "Darth Ron", "Darth Vader","Merlin"});
+    }
+
+    [Fact]
+    public void wizardNames_extension_methods()
+    {
+        // Given
+        var wiz = Queries.wizardNamesEx();
+
+        // Then
+        wiz.Should().BeEquivalentTo(new List<string> { "Gandalf", "Michael Mouse", "Sauron", "Albus Dumbledore", "Harry Potter", "Hermione Granger", "Ron Weasley", "Ron Weasley", "Severus Snape", "Darth Ron", "Darth Vader","Merlin"});
     }
 }
--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -3,14 +3,96 @@ namespace Assignment2;
 
 public class Queries
 {
-    public static IEnumerable<string> wizardsByRowlingLinq() {
+    public static IEnumerable<string> wizardsByRowlingLinq()
+    {
         WizardCollection wizardCol = WizardCollection.Create();
-        
-        //var wizards = wizardCol.GetEnumerator();
+
         var wizNames = from c in wizardCol
-                where c.Creator.Contains("Rowling")
-                select new { Name = c.Name };
-                
-        foreach(var w in wizNames) yield return w.Name;
+                       where c.Creator.Contains("Rowling")
+                       select new { Name = c.Name };
+
+        foreach (var w in wizNames) yield return w.Name;
     }
+
+    public static IEnumerable<string> wizardsByRowlingEx()
+    {
+        WizardCollection wizardCol = WizardCollection.Create();
+
+        var wizNames = wizardCol
+                .Where(c => c.Creator.Contains("Rowling"))
+                .Select(c => new { Name = c.Name });
+
+        foreach (var w in wizNames) yield return w.Name;
+    }
+
+    public static int firstSithLordLinq()
+    {
+        WizardCollection wizardCol = WizardCollection.Create();
+
+        var year = (from c in wizardCol
+            where c.Name.Contains("Darth")
+            orderby c.Year
+            select new {c.Year}).First();
+
+        return year.Year!.Value;
+    }
+
+    public static int firstSithLordEx()
+    {
+        WizardCollection wizardCol = WizardCollection.Create();
+
+        var year = wizardCol
+            .Where(c => c.Name.Contains("Darth"))
+            .MinBy(c => c.Year)!.Year;
+
+        return year!.Value;
+    }
+
+    public static IEnumerable<(string, int)> uniqueListHarryPotterLinq()
+    {
+        WizardCollection wizardCol = WizardCollection.Create();
+
+        var wizNames = (from c in wizardCol
+                       where c.Medium.Contains("Harry Potter")
+                       select new { Name = c.Name, Year = c.Year }).Distinct();
+
+        foreach (var w in wizNames) yield return (w.Name, w.Year!.Value);
+    }
+
+    public static IEnumerable<(string, int)> uniqueListHarryPotterEx()
+    {
+        WizardCollection wizardCol = WizardCollection.Create();
+
+        var wizNames = wizardCol
+                .Where(c => c.Medium.Contains("Harry Potter"))
+                .Select(c => new { Name = c.Name, Year = c.Year })
+                .Distinct();
+
+        foreach (var w in wizNames) yield return (w.Name, w.Year!.Value);
+    }
+
+    public static IEnumerable<string> wizardNamesLinq()
+    {
+        WizardCollection wizardCol = WizardCollection.Create();
+
+        var wizNames = from c in wizardCol
+                       orderby c.Creator descending, c.Name
+                       select new { Name = c.Name };
+
+        foreach (var w in wizNames) yield return w.Name;
+    }
+
+    public static IEnumerable<string> wizardNamesEx()
+    {
+        WizardCollection wizardCol = WizardCollection.Create();
+
+        var wizNames = wizardCol
+                .OrderByDescending(c => c.Creator)
+                .ThenBy(c => c.Name)
+                .Select(c => new { Name = c.Name });
+
+        foreach (var w in wizNames) yield return w.Name;
+    }
+
+
 }
