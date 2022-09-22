0b1b3055018ae254594fcdc2178b283009b2ec8b
Philip Mørch <mrphil2105@gmail.com>
2022-09-22 14:29:03 +0200

    Use camel casing for variables

--- Assignment2.Tests/ImmutableStudentTests.cs
+++ Assignment2.Tests/ImmutableStudentTests.cs
@@ -1,35 +1,34 @@
-namespace Assignment2.Tests;
 using Assignment2.Models;
+
+namespace Assignment2.Tests;
 
 public class ImmutableStudentTests
 {
     [Fact]
     public void Test_2_identical_students_records_equal()
     {
-        var ImmutableStudent = new ImmutableStudent { Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2018, 08, 27), EndDate = new DateTime(2019, 06, 15) };
+        var immutableStudent = new ImmutableStudent { Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2018, 08, 27), EndDate = new DateTime(2019, 06, 15) };
 
-        var ImmutableStudent2 = new ImmutableStudent { Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2018, 08, 27), EndDate = new DateTime(2019, 06, 15) };
+        var immutableStudent2 = new ImmutableStudent { Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2018, 08, 27), EndDate = new DateTime(2019, 06, 15) };
 
-        ImmutableStudent.Equals(ImmutableStudent2).Should().Be(true);
+        immutableStudent.Equals(immutableStudent2).Should().Be(true);
     }
 
     [Fact]
     public void Test_2_identical_students_class_not_equal()
     {
-        var Student = new Student { Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2018, 08, 27), EndDate = new DateTime(2019, 06, 15) };
+        var student = new Student { Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2018, 08, 27), EndDate = new DateTime(2019, 06, 15) };
 
-        var Student2 = new Student { Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2018, 08, 27), EndDate = new DateTime(2019, 06, 15) };
+        var student2 = new Student { Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2018, 08, 27), EndDate = new DateTime(2019, 06, 15) };
 
-        Student.Equals(Student2).Should().Be(false);
+        student.Equals(student2).Should().Be(false);
     }
 
     [Fact]
     public void Test_record_tostring()
     {
-        var ImmutableStudent = new ImmutableStudent { Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2018, 08, 27), EndDate = new DateTime(2019, 06, 15) };
+        var immutableStudent = new ImmutableStudent { Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2018, 08, 27), EndDate = new DateTime(2019, 06, 15) };
 
-        ImmutableStudent.ToString().Should().Be("ImmutableStudent { Id = 12123, GivenName = Bent, Surname = Peter, Status = Dropout, StartDate = 27/08/2018 00:00:00, EndDate = 15/06/2019 00:00:00, GraduationDate = 01/01/1970 00:00:00 }");
+        immutableStudent.ToString().Should().Be("ImmutableStudent { Id = 12123, GivenName = Bent, Surname = Peter, Status = Dropout, StartDate = 27/08/2018 00:00:00, EndDate = 15/06/2019 00:00:00, GraduationDate = 01/01/1970 00:00:00 }");
     }
-
-
 }
--- Assignment2.Tests/StudentTests.cs
+++ Assignment2.Tests/StudentTests.cs
@@ -1,51 +1,46 @@
-namespace Assignment2.Tests;
 using Assignment2.Models;
+
+namespace Assignment2.Tests;
 
 public class StudentTests
 {
     [Fact]
     public void TestIfStudentIsDropout()
     {
-        var Student = new Student{Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2018, 08, 27),EndDate = new DateTime(2019, 06, 15)};
+        var student = new Student{Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2018, 08, 27),EndDate = new DateTime(2019, 06, 15)};
 
-    
-        Assert.Equal(Status.Dropout, Student.Status);
+        Assert.Equal(Status.Dropout, student.Status);
     }
 
     [Fact]
     public void TestIfStudentIsActive()
     {
-        var Student = new Student{Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2018, 08, 27)};
+        var student = new Student{Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2018, 08, 27)};
 
-    
-        Assert.Equal(Status.Active, Student.Status);
+        Assert.Equal(Status.Active, student.Status);
     }
 
     [Fact]
     public void TestIfStudentIsNew()
     {
-        var Student = new Student{Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2023, 08, 27)};
+        var student = new Student{Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2023, 08, 27)};
 
-    
-        Assert.Equal(Status.New, Student.Status);
+        Assert.Equal(Status.New, student.Status);
     }
 
     [Fact]
     public void TestIfStudentIsGraduated()
     {
-        var Student = new Student{Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2018, 08, 27),GraduationDate = new DateTime(2021, 06, 15)};
+        var student = new Student{Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2018, 08, 27),GraduationDate = new DateTime(2021, 06, 15)};
 
-    
-        Assert.Equal(Status.Graduated, Student.Status);
+        Assert.Equal(Status.Graduated, student.Status);
     }
 
-
-        [Fact]
+    [Fact]
     public void TestToStringMethod()
     {
-        var Student = new Student{Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2018, 08, 27),GraduationDate = new DateTime(2021, 06, 15)};
+        var student = new Student{Id = 12123, GivenName = "Bent", Surname = "Peter", StartDate = new DateTime(2018, 08, 27),GraduationDate = new DateTime(2021, 06, 15)};
 
-    
-        Assert.Equal("Id: 12123, GivenName: Bent, Surname: Peter, Status: Graduated, StartDate: 27/08/2018 00:00:00, EndDate: 01/01/1970 00:00:00, GraduationDate: 15/06/2021 00:00:00", Student.ToString());
+        Assert.Equal("Id: 12123, GivenName: Bent, Surname: Peter, Status: Graduated, StartDate: 27/08/2018 00:00:00, EndDate: 01/01/1970 00:00:00, GraduationDate: 15/06/2021 00:00:00", student.ToString());
     }
 }
